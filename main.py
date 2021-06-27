6e534c6aa1b61feeec207711826c665d6d60eb1a
ali114323 <84106340+ali114323@users.noreply.github.com>
2021-06-27 11:36:41 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
+import cv2
 import random
-w = open("words.txt").readlines()
-a = random.choice(w)
-guesses = 10
-won = False
-incorrect = []
-correct = []
-print("Hardest hangman ever! There are over 2000 words!")
-print("You only have 6 guesses.")
-print("Correct guesses are in random order")
-input("Press enter to begin...")
-print("Your word is " + str(len(a) - 1) + " characters long")
-while guesses >0:
-    if guesses < 11:
-        guess = input("What is your guess? ")
-        b = a.find(str(guess))
-        if guess in a:
-            print("Correct!")
-            a = a.replace(guess, "")
-            correct.append(guess)
-            print("Correct guesses: " + str(correct))
-            print("The word has " + str(len(a) - 1) + " characters left")
-        else:
-            print("Wrong!")
-            guesses -= 1
-            incorrect.append(guess)
-            print("Incorrect guesses: " + str(incorrect))
-            print("You have " + str(guesses) + " guesses remaining.")
-        if len(a) == 1:
-            won = True
-            print("You win!")
-            input("Press here to exit...")
-        if won == True:
-            break
-if guesses == 0:
-    print("You lose!")
-    input("Press enter to exit...")
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_blue = np.array([36,0,0])
+    upper_blue = np.array([86,255,255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    result = cv2.bitwise_and(frame,frame,mask=mask)
+    cv2.imshow('Webcam Go Brr', result)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
